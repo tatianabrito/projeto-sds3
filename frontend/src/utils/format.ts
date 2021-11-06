92afd36ed4852117c2de08bff0071c087078854c
Tatiana Brito <brito.tatiana@gmail.com>
2021-11-06 07:26:35 -0300

    corrigindo package

--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -10,3 +10,4 @@ export const formatLocalDate = (date: string, pattern: string) => {
     const dtDateOnly = new Date(dt.valueOf() + dt.getTimezoneOffset() * 60 * 1000);
     return format(dtDateOnly, pattern);
 }
+
